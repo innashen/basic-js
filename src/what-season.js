36f5111009b23f1df1e954b5cdbdcec37c0877c5
innashen <101499971+innashen@users.noreply.github.com>
2022-10-17 22:39:49 +0300

    feat:done

--- src/what-season.js
+++ src/what-season.js
@@ -16,9 +16,9 @@ if(date instanceof Date){
     const month = date.getMonth();
     if(month === null || month === undefined) { return 'Invalid date!'}
     else if(month === 1 || month === 0 || month === 11){ return 'winter';}
-    else if(2 <= month && month <= 4){ return 'spring';}
-    else if(5 <= month && month <= 7){ return 'summer';}
-    else if(8 <= month && month <= 10) { return 'autumn';}
+    else if(3 <= month && month <= 5){ return 'spring';}
+    else if(6 <= month && month <= 8){ return 'summer';}
+    else if(9 <= month && month <= 11) { return 'autumn';}
   }
 }
 
